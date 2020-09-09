d50c646d009be47bc4a8618a579428b6a66c5d21
Luis Augusto <luis.louback@yahoo.com>
2020-09-09 15:33:57 -0300

    Projeto Visual Studio Aula Git Hub

---
+++ Aulas_Udemy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aulas_Udemy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int A ,B ;
+			
+			Console.WriteLine("Digite o valor de A:");
+			A = int.Parse(Console.ReadLine());
+			
+			Console.WriteLine("Digite o valor de B:");
+			B = int.Parse(Console.ReadLine());
+
+			if (A > B)
+				Console.WriteLine("O maior número é " + A);
+			else if (B > A)
+				Console.WriteLine("O maior número é " + B);
+		}
+	}
+}
