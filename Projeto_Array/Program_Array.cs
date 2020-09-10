1218792dc83cf95ef13affff1c02cdb118e047ae
Luis Augusto CL Claudino <luis.louback@yahoo.com>
2020-09-10 12:01:53 -0300

    Novo Projeto C#, com Array e tambem o metodo de Maior Numero.

---
+++ Projeto_Array/Program_Array.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Projeto_Array
+{
+    class Program
+    {
+        static void MaiorNumero(){
+
+            int numeroA , numeroB;
+			Console.WriteLine("Digite o valor de A:");
+			numeroA = int.Parse(Console.ReadLine());
+			
+			Console.WriteLine("Digite o valor de B:");
+			numeroB = int.Parse(Console.ReadLine());
+
+			if (numeroA > numeroB)
+				Console.WriteLine("O maior número é " + numeroA);
+			else if (numeroB > numeroA)
+				Console.WriteLine("O maior número é " + numeroB);
+			else if (numeroB == numeroA)
+				Console.WriteLine("Ambos números são iguais !");
+
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro vamos fazer o teste de maior número");
+            //MaiorNumero();  
+
+            String [] array ;
+
+            Console.WriteLine("Digite os numero que vc quer no array separados por ( , )");
+            
+            if ()
+            {
+                
+            }
+            
+            array = Console.ReadLine().Split(",");
+
+            Console.WriteLine("Os numeros que você escolheu foram :");
+
+            Console.Write("{");
+            for (int i = 0; i < array.Length; i++)
+            {                
+                Console.Write(array[i]+",");
+            }
+            Console.Write("}");
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
